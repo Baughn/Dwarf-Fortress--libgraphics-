96726cf4ce0fcd00aa1e3aff54fd1eb207228d64
Svein Ove Aas <svein@sabin.home.brage.info>
2009-07-06 16:44:45 +0200

    Updated update-head for d13

--- update-head.sh
+++ update-head.sh
@@ -3,9 +3,9 @@ set -e
 
 mkdir df_linux
 mkdir df_linux/libs
-scons
+schroot -- scons -j2
 cp libs/libgraphics.so df_linux/libs
-tar cjf df-40d12-head.tar.bz2 df_linux
-scp df-40d12-head.tar.bz2 brage.info:public_html/
-rm df-40d12-head.tar.bz2
+tar cjf df-40d13-head.tar.bz2 df_linux
+scp df-40d13-head.tar.bz2 brage.info:public_html/
+rm df-40d13-head.tar.bz2
 rm -r df_linux
