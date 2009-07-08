64b24c58aa4508a4558562af244b532f75f18932
Svein Ove Aas <svein.ove@aas.no>
2009-07-08 13:25:45 +0200

    update-head.sh: Clean up after yourself

--- update-head.sh
+++ update-head.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -e
 
+(
+set -e
 mkdir df_linux
 mkdir df_linux/libs
 schroot -- scons -j2
@@ -8,4 +9,5 @@ cp libs/libgraphics.so df_linux/libs
 tar cjf df-40d13-head.tar.bz2 df_linux
 scp df-40d13-head.tar.bz2 brage.info:public_html/
 rm df-40d13-head.tar.bz2
+)
 rm -r df_linux
