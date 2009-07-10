4b0f755478a69f846b2abe3f51b5bd0686eab19a
Svein Ove Aas <svein.ove@aas.no>
2009-07-11 01:25:48 +0200

    Include libstdc++ in update-head

--- update-head.sh
+++ update-head.sh
@@ -5,7 +5,8 @@ set -e
 mkdir df_linux
 mkdir df_linux/libs
 schroot -- scons -j2
-cp libs/libgraphics.so df_linux/libs
+cp libs/libgraphics.so df_linux/libs/
+schroot cp /usr/lib/libstdc++.so.6 df_linux/libs/
 tar cjf df-40d13-head.tar.bz2 df_linux
 scp df-40d13-head.tar.bz2 brage.info:public_html/
 rm df-40d13-head.tar.bz2
