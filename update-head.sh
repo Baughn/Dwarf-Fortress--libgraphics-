c3c68e7dd6b14615bc617a4fff67a96795106d89
Svein Ove Aas <svein.ove@aas.no>
2009-08-30 18:27:00 +0200

    Updated update-head.sh

--- update-head.sh
+++ update-head.sh
@@ -7,8 +7,8 @@ mkdir df_linux/libs
 schroot -- scons -j2
 cp libs/libgraphics.so df_linux/libs/
 schroot cp /usr/lib/libstdc++.so.6 df_linux/libs/
-tar cjf df-40d14-head.tar.bz2 df_linux
-scp df-40d14-head.tar.bz2 brage.info:public_html/
-rm df-40d14-head.tar.bz2
+tar cjf df-40d15-head.tar.bz2 df_linux
+scp df-40d15-head.tar.bz2 brage.info:public_html/
+rm df-40d15-head.tar.bz2
 )
 rm -r df_linux
