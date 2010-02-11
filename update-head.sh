f9106edfe001ac24043f48fd49df531d266ab7c1
Svein Ove Aas <svein.ove@aas.no>
2010-02-11 21:34:52 +0100

    makes update-head work inside schroot

--- update-head.sh
+++ update-head.sh
@@ -2,11 +2,18 @@
 
 (
 set -e
+
+if [ ! -d /opt/arch32 ]; then
+  SCHROOT=""
+else
+  SCHROOT="schroot --"
+fi
+
 mkdir df_linux
 mkdir df_linux/libs
-schroot -- scons -j2
+$SCHROOT scons -j2
 cp -d libs/* df_linux/libs/
-schroot cp /usr/lib/libstdc++.so.6 df_linux/libs/
+$SCHROOT cp /usr/lib/libstdc++.so.6 df_linux/libs/
 tar cjf df-40d17-head.tar.bz2 df_linux
 scp df-40d17-head.tar.bz2 brage.info:public_html/
 rm df-40d17-head.tar.bz2
