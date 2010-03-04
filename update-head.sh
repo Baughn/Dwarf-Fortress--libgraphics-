ee57af57855c4b356ed4bfe17b7f8dcb1966696e
Svein Ove Aas <svein.ove@aas.no>
2010-03-05 00:07:31 +0100

    Import d19

--- update-head.sh
+++ update-head.sh
@@ -17,8 +17,8 @@ cp dwarfort.exe df_linux/
 cp data/shader.* df_linux/data/
 cp data/init/init.txt df_linux/data/init/
 $SCHROOT cp /usr/lib/libstdc++.so.6 df_linux/libs/
-tar cjf df-40d18-head.tar.bz2 df_linux
-scp df-40d18-head.tar.bz2 brage.info:public_html/
-rm df-40d18-head.tar.bz2
+tar cjf df-40d19-head.tar.bz2 df_linux
+scp df-40d19-head.tar.bz2 brage.info:public_html/
+rm df-40d19-head.tar.bz2
 )
 rm -r df_linux
