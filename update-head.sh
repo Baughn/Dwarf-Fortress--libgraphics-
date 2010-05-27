a12f803f60ee327eae5b60d33a5ca3a115867bcc
Svein Ove Aas <svein.ove@aas.no>
2010-05-27 14:32:54 +0200

    Update update-head

--- update-head.sh
+++ update-head.sh
@@ -14,11 +14,11 @@ mkdir -p df_linux/libs
 mkdir -p df_linux/data/init
 cp -d libs/* df_linux/libs/
 cp dwarfort.exe df_linux/
-cp data/shader.* df_linux/data/
-cp data/init/init.txt df_linux/data/init/
+#cp data/shader.* df_linux/data/
+#cp data/init/init.txt df_linux/data/init/
 $SCHROOT cp /usr/lib/libstdc++.so.6 df_linux/libs/
-tar cjf df-40d19-head.tar.bz2 df_linux
-scp df-40d19-head.tar.bz2 brage.info:public_html/
-rm df-40d19-head.tar.bz2
+tar cjf df-head.tar.bz2 df_linux
+scp df-head.tar.bz2 brage.info:public_html/
+rm df-head.tar.bz2
 )
 rm -r df_linux
